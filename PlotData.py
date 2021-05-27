23854664306aecee67ebd9d0a30e619f6b3e87e2
Odilon Thioux <o.d.thioux@student.rug.nl>
2021-05-27 12:42:55 +0200

    Data Plots

---
+++ PlotData.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+pos_dat = np.load("NN_projectPosDat.npy")
+y = pos_dat[1000:2000, 1]
+x = pos_dat[1000:2000, 0]
+plt.scatter(x, y)
+plt.ylim([0,500])
+plt.xlim([50,600])
+plt.show()
